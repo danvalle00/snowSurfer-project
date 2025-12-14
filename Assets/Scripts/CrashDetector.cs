8cf46c3b13728489df8ad289bd5bd2cddc4a8426
yhr <danieldovalle10@gmail.com>
2025-12-14 01:24:39 -0300

    Lessons 41-45

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayBeforeRestart = 1f;
+    [SerializeField] private ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
         {
-            Debug.Log("Crash Detected!");
+            crashEffect.Play();
+            Invoke("RestartLevel", delayBeforeRestart);
         }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine/FinishLine.cs
+++ Assets/Scripts/FinishLine/FinishLine.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayBeforeRestart = 1f;
+    [SerializeField] private ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if (collision.gameObject.layer == layerIndex)
         {
-            Debug.Log("Finish Line Reached!");
+            finishEffect.Play();
+            Invoke("RestartLevel", delayBeforeRestart);
         }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
